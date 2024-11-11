539664640687ae3c819ed76ce56706ef80604cb1
Joe Hosteny <jhosteny@gmail.com>
2024-11-11 14:34:19 -0500

    fix: optional group header parsing

--- lib/ex_ark/serdes/optional_group_header.ex
+++ lib/ex_ark/serdes/optional_group_header.ex
@@ -14,18 +14,14 @@ defmodule ExArk.Serdes.OptionalGroupHeader do
   end
 
   #
-  # +----------------+----------------+------------------+------------------+
-  # | Magic (4 bits) | Groups (1 bit) | Sections (1 bit) | Version (2 bits) |
-  # +----------------+----------------+------------------+------------------+
+  # +----------------+----------------+------------------+-----------------+
+  # | Magic (4 bits) | Unused (1 bit) | Sections (1 bit) | Unused (2 bits) |
+  # +----------------+----------------+------------------+-----------------+
   #
 
   @magic 0xE
-  @version 0x1
-  @groups 0x0
 
-  def read(
-        %InputStream{bytes: <<@magic::4, @groups::1, sections::1, @version::2, rest::binary>>, offset: offset} = stream
-      ) do
+  def read(%InputStream{bytes: <<@magic::4, 0::1, sections::1, 0::2, rest::binary>>, offset: offset} = stream) do
     stream = %{stream | bytes: rest, offset: offset + 1, has_more_sections: sections != 0}
 
     with {:ok, %Result{stream: stream, reified: identifier}} <- Primitives.read(:uint8, stream),
@@ -44,12 +40,8 @@ defmodule ExArk.Serdes.OptionalGroupHeader do
     end
   end
 
-  def read(%InputStream{bytes: <<_magic::4, @groups::1, _sections::1, @version::2, _rest::binary>>}),
+  def read(%InputStream{bytes: <<_magic::4, 0::1, _sections::1, 0::2, _rest::binary>>}),
     do: {:error, :bad_magic}
 
-  def read(%InputStream{bytes: <<@magic::4, @groups::1, _sections::1, _version::2, _rest::binary>>}),
-    do: {:error, :bad_version}
-
-  def read(%InputStream{bytes: <<@magic::4, _groups::1, _sections::1, @version::2, _rest::binary>>}),
-    do: {:error, :bad_groups}
+  def read(%InputStream{bytes: <<@magic::4, _::1, _sections::1, _::2, _rest::binary>>}), do: {:error, :bad_header}
 end
