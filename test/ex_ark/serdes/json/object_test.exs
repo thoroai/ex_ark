01d7952ad103bc826354a32e5c6bed57a3d1647d
Joe Hosteny <jhosteny@users.noreply.github.com>
2026-01-28 15:13:16 -0500

    fix: deserialize JSON with missing groups properly (#65)

--- test/ex_ark/serdes/json/object_test.exs
+++ test/ex_ark/serdes/json/object_test.exs
@@ -18,6 +18,142 @@ defmodule ExArk.Serdes.Json.ObjectTest do
       assert deserialized == data
     end
 
+    test "optional groups - deserializing old data without group fields", %{registry: registry} do
+      # Simulate old schema data that doesn't have the group fields
+      old_json = Jason.encode!(%{"id" => "test-id", "value" => 42})
+      type = "ex_ark::test::ObjectWithOptionalGroup"
+
+      # Should succeed - missing group should be treated as optional
+      {:ok, deserialized} = ExArk.read_object_from_json(registry, type, old_json)
+
+      # Should have the regular fields but not the group fields
+      assert deserialized.id == "test-id"
+      assert deserialized.value == 42
+      assert Map.has_key?(deserialized, :group_field_1) == false
+      assert Map.has_key?(deserialized, :group_field_2) == false
+    end
+
+    test "optional groups - deserializing new data with group fields", %{registry: registry} do
+      # New schema data that includes the group fields
+      new_json =
+        Jason.encode!(%{
+          "id" => "test-id",
+          "value" => 42,
+          "group_field_1" => "group-value",
+          "group_field_2" => 99
+        })
+
+      type = "ex_ark::test::ObjectWithOptionalGroup"
+
+      {:ok, deserialized} = ExArk.read_object_from_json(registry, type, new_json)
+
+      # Should have all fields
+      assert deserialized.id == "test-id"
+      assert deserialized.value == 42
+      assert deserialized.group_field_1 == "group-value"
+      assert deserialized.group_field_2 == 99
+    end
+
+    test "optional groups - roundtrip with group fields", %{registry: registry} do
+      data = %{
+        id: "test-id",
+        value: 42,
+        group_field_1: "group-value",
+        group_field_2: 99
+      }
+
+      type = "ex_ark::test::ObjectWithOptionalGroup"
+
+      {:ok, serialized} = ExArk.write_object_to_json(registry, type, data)
+      {:ok, deserialized} = ExArk.read_object_from_json(registry, type, serialized)
+
+      assert deserialized == data
+    end
+
+    test "optional fields - missing optional fields", %{registry: registry} do
+      # JSON with only required fields, no optional fields
+      json = Jason.encode!(%{"required_field" => "test", "another_required_field" => 42})
+      type = "ex_ark::test::ObjectWithOptionalFields"
+
+      {:ok, deserialized} = ExArk.read_object_from_json(registry, type, json)
+
+      # Should have required fields but not optional fields
+      assert deserialized.required_field == "test"
+      assert deserialized.another_required_field == 42
+      assert Map.has_key?(deserialized, :optional_string) == false
+      assert Map.has_key?(deserialized, :optional_int) == false
+    end
+
+    test "optional fields - present optional fields", %{registry: registry} do
+      json =
+        Jason.encode!(%{
+          "required_field" => "test",
+          "optional_string" => "optional-value",
+          "optional_int" => 99,
+          "another_required_field" => 42
+        })
+
+      type = "ex_ark::test::ObjectWithOptionalFields"
+
+      {:ok, deserialized} = ExArk.read_object_from_json(registry, type, json)
+
+      # Should have all fields
+      assert deserialized.required_field == "test"
+      assert deserialized.optional_string == "optional-value"
+      assert deserialized.optional_int == 99
+      assert deserialized.another_required_field == 42
+    end
+
+    test "optional fields - partial optional fields", %{registry: registry} do
+      # Only one of the optional fields is present
+      json =
+        Jason.encode!(%{
+          "required_field" => "test",
+          "optional_string" => "optional-value",
+          "another_required_field" => 42
+        })
+
+      type = "ex_ark::test::ObjectWithOptionalFields"
+
+      {:ok, deserialized} = ExArk.read_object_from_json(registry, type, json)
+
+      # Should have required fields and one optional field
+      assert deserialized.required_field == "test"
+      assert deserialized.optional_string == "optional-value"
+      assert Map.has_key?(deserialized, :optional_int) == false
+      assert deserialized.another_required_field == 42
+    end
+
+    test "optional fields - roundtrip with optional fields", %{registry: registry} do
+      data = %{
+        required_field: "test",
+        optional_string: "optional-value",
+        optional_int: 99,
+        another_required_field: 42
+      }
+
+      type = "ex_ark::test::ObjectWithOptionalFields"
+
+      {:ok, serialized} = ExArk.write_object_to_json(registry, type, data)
+      {:ok, deserialized} = ExArk.read_object_from_json(registry, type, serialized)
+
+      assert deserialized == data
+    end
+
+    test "optional fields - roundtrip without optional fields", %{registry: registry} do
+      data = %{
+        required_field: "test",
+        another_required_field: 42
+      }
+
+      type = "ex_ark::test::ObjectWithOptionalFields"
+
+      {:ok, serialized} = ExArk.write_object_to_json(registry, type, data)
+      {:ok, deserialized} = ExArk.read_object_from_json(registry, type, serialized)
+
+      assert deserialized == data
+    end
+
     test "final roundtrip", %{registry: registry} do
       data = %{field: %{"key1" => 42, "key2" => 74}}
       type_final = "ex_ark::test::FinalObject"
