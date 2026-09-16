217524e84b94b0eae47cb43fcc20c91d2f174b5c
olafbado <niebiel1@gmail.com>
2026-09-16 15:22:35 +0200

    Enhance dictionary serialization and deserialization

--- lib/ex_ark/serdes/json/fields/dictionary.ex
+++ lib/ex_ark/serdes/json/fields/dictionary.ex
@@ -13,15 +13,32 @@ defmodule ExArk.Serdes.Json.Fields.Dictionary do
 
   require Logger
 
+  @object_key_types ["string", "guid", "enum"]
+
+  @typep pair :: {term(), term()}
+  @typep pairs :: [pair()] | %{term() => term()}
+  @typep encoded :: %{optional(term()) => term()} | [[term()]]
+
   @spec read(Reader.t(), Field.t(), Registry.t()) :: {:ok, ReaderResult.t()} | Json.deserialization_failure()
   def read(%Reader{decoded: nil} = _reader, %Field{} = _field, %Registry{} = _registry) do
     {:ok, %ReaderResult{reified: %{}}}
   end
 
   def read(%Reader{} = reader, %Field{} = field, %Registry{} = registry) do
-    read_items(reader.decoded, field, registry)
+    reader.decoded
+    |> to_pairs(field)
+    |> read_items(field, registry)
   end
 
+  # Transforms both wire shapes so they enumerate as {key, value}. A decoded object
+  # %{"log" => config} already does and is returned untouched, an array of pairs [[1, config]]
+  # does not, so it becomes a list of tuples.
+  @spec to_pairs(term(), Field.t()) :: pairs()
+  defp to_pairs(decoded, %Field{} = field) do
+    if encoded_as_object?(field), do: decoded, else: Enum.map(decoded, fn [key, value] -> {key, value} end)
+  end
+
+  @spec read_items(pairs(), Field.t(), Registry.t()) :: {:ok, ReaderResult.t()} | Json.deserialization_failure()
   defp read_items(items, %Field{} = field, %Registry{} = registry) do
     # NOTE: we reify a list here, since we will convert this to a map at the
     # end of the operation.
@@ -29,7 +46,7 @@ defmodule ExArk.Serdes.Json.Fields.Dictionary do
 
     items
     |> Enum.with_index(fn elt, idx -> {idx, elt} end)
-    |> Enum.reduce_while(reply, fn {i, [key, value]}, {_, result} ->
+    |> Enum.reduce_while(reply, fn {i, {key, value}}, {_, result} ->
       with {:ok, %ReaderResult{reified: key}} <- Fields.read(%Reader{decoded: key}, field.ctr_key_type, registry),
            {:ok, %ReaderResult{reified: value}} <-
              Fields.read(%Reader{decoded: value}, field.ctr_value_type, registry) do
@@ -54,7 +71,7 @@ defmodule ExArk.Serdes.Json.Fields.Dictionary do
 
         with {:ok, %WriterResult{encoded: key}} <- Fields.write(field.ctr_key_type, key, registry),
              {:ok, %WriterResult{encoded: value}} <- Fields.write(field.ctr_value_type, value, registry) do
-          {:cont, {:ok, %WriterResult{result | encoded: [[key, value]] ++ acc}}}
+          {:cont, {:ok, %WriterResult{result | encoded: [{key, value}] ++ acc}}}
         else
           {:error, name, context} ->
             Logger.error(
@@ -71,10 +88,23 @@ defmodule ExArk.Serdes.Json.Fields.Dictionary do
         {:ok, %WriterResult{encoded: nil}}
 
       {:ok, %WriterResult{encoded: encoded}} ->
-        {:ok, %WriterResult{encoded: Enum.reverse(encoded)}}
+        {:ok, %WriterResult{encoded: encoded |> Enum.reverse() |> from_pairs(field)}}
 
       other ->
         other
     end
   end
+
+  # Turns the list of {key, value} tuples into a map, or back into a list of [key, value] pairs.
+  @spec from_pairs([pair()], Field.t()) :: encoded()
+  defp from_pairs(items, %Field{} = field) do
+    if encoded_as_object?(field), do: Map.new(items), else: Enum.map(items, fn {key, value} -> [key, value] end)
+  end
+
+  # JSON allows only strings as keys. A string, guid or enum key is written as a string, so ark
+  # uses it as the key. Any other key, an int or a whole object, cannot be written as a key, so
+  # ark writes a list of [key, value] pairs instead. Which one applies is read from the key type
+  # the schema declares for this dictionary, `ctr_key_type`.
+  @spec encoded_as_object?(Field.t()) :: boolean()
+  defp encoded_as_object?(%Field{ctr_key_type: %Field{type: type}}), do: type in @object_key_types
 end
--- test/ex_ark/serdes/json/dictionary_test.exs
+++ test/ex_ark/serdes/json/dictionary_test.exs
@@ -2,6 +2,8 @@ defmodule ExArk.Serdes.Json.Fields.DictionaryTest do
   use ExUnit.Case, async: true
   alias ExArk
 
+  @product_config_type "ex_ark::test::ProductConfig"
+
   setup do
     registry = ExArk.load_schemas!("test/fixtures/ir/dictionaries.ir")
     {:ok, %{registry: registry}}
@@ -44,5 +46,104 @@ defmodule ExArk.Serdes.Json.Fields.DictionaryTest do
 
       assert deserialized == data
     end
+  end
+
+  describe "dictionary wire format" do
+    test "writes a string-keyed dictionary as a json object", %{registry: registry} do
+      {:ok, serialized} = ExArk.write_object_to_json(registry, @product_config_type, product_config())
+
+      assert Jason.decode!(serialized) == json_product_config()
+    end
+
+    test "reads a string-keyed dictionary written as a json object", %{registry: registry} do
+      serialized = Jason.encode!(json_product_config())
+
+      assert {:ok, data} = ExArk.read_object_from_json(registry, @product_config_type, serialized)
+      assert data == product_config()
+    end
+
+    test "writes a guid-keyed dictionary as a json object", %{registry: registry} do
+      data = %{dictionary: %{"3f2504e0-4f89-11d3-9a0c-0305e82c3301" => 1}}
+
+      {:ok, serialized} = ExArk.write_object_to_json(registry, "ex_ark::test::GuidToIntDictionary", data)
+
+      assert Jason.decode!(serialized) == %{"dictionary" => %{"3f2504e0-4f89-11d3-9a0c-0305e82c3301" => 1}}
+    end
+
+    test "reads a guid-keyed dictionary written as a json object", %{registry: registry} do
+      serialized = ~s({"dictionary":{"3f2504e0-4f89-11d3-9a0c-0305e82c3301":1}})
+
+      assert {:ok, data} = ExArk.read_object_from_json(registry, "ex_ark::test::GuidToIntDictionary", serialized)
+      assert data == %{dictionary: %{"3f2504e0-4f89-11d3-9a0c-0305e82c3301" => 1}}
+    end
+
+    test "writes an enum-keyed dictionary as a json object keyed by the enum name", %{registry: registry} do
+      data = %{dictionary: %{Red: 1, Green: 2}}
+
+      {:ok, serialized} = ExArk.write_object_to_json(registry, "ex_ark::test::EnumToIntDictionary", data)
+
+      assert Jason.decode!(serialized) == %{"dictionary" => %{"Red" => 1, "Green" => 2}}
+    end
+
+    test "reads an enum-keyed dictionary written as a json object", %{registry: registry} do
+      serialized = ~s({"dictionary":{"Red":1,"Green":2}})
+
+      assert {:ok, data} = ExArk.read_object_from_json(registry, "ex_ark::test::EnumToIntDictionary", serialized)
+      assert data == %{dictionary: %{Red: 1, Green: 2}}
+    end
+
+    test "writes an int-keyed dictionary as an array of pairs", %{registry: registry} do
+      data = %{dictionary: %{1 => 10, 2 => 20}}
+
+      {:ok, serialized} = ExArk.write_object_to_json(registry, "ex_ark::test::IntToIntDictionary", data)
+
+      assert Jason.decode!(serialized) == %{"dictionary" => [[1, 10], [2, 20]]}
+    end
+
+    test "reads an int-keyed dictionary written as an array of pairs", %{registry: registry} do
+      serialized = ~s({"dictionary":[[1,10],[2,20]]})
+
+      assert {:ok, data} = ExArk.read_object_from_json(registry, "ex_ark::test::IntToIntDictionary", serialized)
+      assert data == %{dictionary: %{1 => 10, 2 => 20}}
+    end
+
+    test "writes null for an empty dictionary", %{registry: registry} do
+      {:ok, serialized} = ExArk.write_object_to_json(registry, @product_config_type, %{categories: %{}})
+
+      assert Jason.decode!(serialized) == %{"categories" => nil}
+    end
+
+    test "reads null as an empty dictionary", %{registry: registry} do
+      assert {:ok, data} = ExArk.read_object_from_json(registry, @product_config_type, ~s({"categories":null}))
+      assert data == %{categories: %{}}
+    end
+  end
+
+  defp product_config do
+    %{
+      categories: %{
+        "log" => %{
+          upload_immediate: true,
+          priority: 0,
+          max_space_in_blocks: 131_072,
+          min_free_space_in_blocks: 16_384,
+          min_age_in_days: 1
+        }
+      }
+    }
+  end
+
+  defp json_product_config do
+    %{
+      "categories" => %{
+        "log" => %{
+          "upload_immediate" => true,
+          "priority" => 0,
+          "max_space_in_blocks" => 131_072,
+          "min_free_space_in_blocks" => 16_384,
+          "min_age_in_days" => 1
+        }
+      }
+    }
   end
 end
